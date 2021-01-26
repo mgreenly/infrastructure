8ad0a43f12a9b708d7a75fc5511643c898b9ca7a
Michael Greenly <mgreenly@logic-refinery.com>
2021-01-26 10:07:23 -0600

    remove k3s folder

--- k3s/README.md
+++
@@ -1,8 +0,0 @@
-
-SETUP
-
-  1. enable legacy iptables
-  2. install server
-  3. fetch server token
-  3. install agents
-  4. prosper
--- k3s/scripts/install-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# $> ssh server.k3s.logic-refinery.io 'sudo bash -s' -- < ./scripts/install-server.sh
-#
-
-curl -sfL https://get.k3s.io | sh -
